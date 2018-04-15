ff3bf81d811893db547a29a5b8604999b08f22de
Lolo <lolo8304@gmail.com>
2018-04-15 20:42:37 +0200

    update

--- pi-cam-cv/test/test_stop_sign_distance.py
+++ pi-cam-cv/test/test_stop_sign_distance.py
@@ -46,7 +46,7 @@ class ObjectDetection(object):
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            flags=cv2.CASCADE_SCALE_IMAGE
         )
 
         # draw a rectangle around the objects
