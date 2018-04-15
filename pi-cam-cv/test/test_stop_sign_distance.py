6a368eb0e0989647eb39e111061ece0682207098
Lorenz Hänggi <lolo8304@gmail.com>
2018-04-15 19:09:47 +0200

    fix

--- pi-cam-cv/test/test_stop_sign_distance.py
+++ pi-cam-cv/test/test_stop_sign_distance.py
@@ -103,6 +103,12 @@ class DetectionHandler():
 
     obj_detection = ObjectDetection()
 
+
+    # h1: stop sign
+    h1 = 15.5 - 10  # cm
+    # h2: traffic light
+    h2 = 15.5 - 10
+
     # cascade classifiers
     stop_cascade = cv2.CascadeClassifier('cascade_xml/stop_sign.xml')
     light_cascade = cv2.CascadeClassifier('cascade_xml/traffic_light.xml')
@@ -114,15 +120,15 @@ class DetectionHandler():
     def handle(self):
 
       # capture frames from the camera
-      for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+      for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
         # grab the raw NumPy array representing the image, then initialize the timestamp
         # and occupied/unoccupied text
         image = frame.array
         grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # object detection
-        v_param1 = self.obj_detection.detect(self.stop_cascade, gray, image)
-        v_param2 = self.obj_detection.detect(self.light_cascade, gray, image)
+        v_param1 = self.obj_detection.detect(self.stop_cascade, grey_image, image)
+        v_param2 = self.obj_detection.detect(self.light_cascade, grey_image, image)
         # distance measurement
         if v_param1 > 0 or v_param2 > 0:
           d1 = self.d_to_camera.calculate(v_param1, self.h1, 300, image)
@@ -139,7 +145,7 @@ class DetectionHandler():
         key = cv2.waitKey(1) & 0xFF
       
         # clear the stream in preparation for the next frame
-        rawCapture.truncate(0)
+        self.rawCapture.truncate(0)
       
         # if the `q` key was pressed, break from the loop
         if key == ord("q"):
